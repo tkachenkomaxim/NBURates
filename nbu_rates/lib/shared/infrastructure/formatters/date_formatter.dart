125e3040b39e4eba1f7fecc13469dfad7ee5c2c4
Maksym Tkachenko <myectyh@gmail.com>
2024-07-12 12:18:20 +0300

    [nbu] fixed date formatter

--- nbu_rates/lib/shared/infrastructure/formatters/date_formatter.dart
+++ nbu_rates/lib/shared/infrastructure/formatters/date_formatter.dart
@@ -2,6 +2,6 @@ import 'package:easy_localization/easy_localization.dart';
 
 class DateFormatter {
   static String formatDate(DateTime dateTime) {
-    return DateFormat('yyyy-MM-dd').format(dateTime);
+    return DateFormat('yyyyMMdd').format(dateTime);
   }
 }
